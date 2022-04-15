d38e89eb58b175613492cc9299c12c379cb7f232
Mun Joon Teo <teomunjoon101@gmail.com>
2022-04-15 22:15:28 +1000

    Fixed expected results for basic constructor test case

--- test/01-testBoardCreate.cpp
+++ test/01-testBoardCreate.cpp
@@ -14,11 +14,12 @@ TEST_CASE("Create board") {
         2594073385365405732,
         4755801206503243842,
         9295429630892703873,
-        576460752303423504,
-        1152921504606846984,
+        576460752303423496,
+        1152921504606846992,
     };
 
     for (int i = 0; i < 8; ++i) {
+        a.printBB(static_cast<enumPiece>(i));
         CHECK(expected[i] == a.getPieceSet(static_cast<enumPiece>(i)));
     }
 }
