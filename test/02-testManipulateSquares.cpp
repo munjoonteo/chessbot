87c283119effd91467d927385a2fb9782ba7e3eb
Mun Joon Teo <teomunjoon101@gmail.com>
2022-04-18 19:00:30 +1000

    Added ULL to end of big numbers to silence warning about large integer constant

--- test/02-testManipulateSquares.cpp
+++ test/02-testManipulateSquares.cpp
@@ -7,9 +7,9 @@
 TEST_CASE("Getting a bitboard") {
     CBoard board = CBoard("8/8/8/8/8/8/8/K6k w - - 0 1");
 
-    CHECK(board.getPieceSet(enumPiece::nKing) == 9295429630892703744);
-    CHECK(board.getPieceSet(enumPiece::nWhite) == 72057594037927936);
-    CHECK(board.getPieceSet(enumPiece::nKing, enumPiece::nWhite) == 72057594037927936);
+    CHECK(board.getPieceSet(enumPiece::nKing) == 9295429630892703744ULL);
+    CHECK(board.getPieceSet(enumPiece::nWhite) == 72057594037927936ULL);
+    CHECK(board.getPieceSet(enumPiece::nKing, enumPiece::nWhite) == 72057594037927936ULL);
 }
 
 TEST_CASE("Manipulating a square") {
